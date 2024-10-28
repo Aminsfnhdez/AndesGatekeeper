b6e2971a240e4e0402464ec34b891516e8d1e79e
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2024-10-27 20:18:42 -0500

    base plantilla administrador

--- src/app/Administrador/features/docente.routes.ts
+++ src/app/Administrador/features/docente.routes.ts
@@ -1,9 +1,13 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
+import LayoutComponent from '../../shared/ui/layout.component';
 
 export default [
-    {
-        path: '',
-        loadComponent: () => import('./lista-docentes/lista-docentes.component')
-    },
-    
-] as Routes
+//   {
+//     path: '',
+//     LoadComponent: () => import('./dashboard/dashboard.component'),
+//   },
+  {
+    path: '',
+    loadComponent: () => import('./lista-docentes/lista-docentes.component'),
+  },
+] as Routes;
